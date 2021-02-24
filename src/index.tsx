9bace2d4ea6c5192984d6133ac41d8c3c9979867
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-02-24 15:31:22 +0200

    get esbuild-wasm from unpkg.com

--- src/index.tsx
+++ src/index.tsx
@@ -17,7 +17,7 @@ const App: React.FC = () => {
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: "/esbuild.wasm",
+      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
     });
   };
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {

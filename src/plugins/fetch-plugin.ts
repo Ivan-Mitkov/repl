76f37c38bc6be0d8179a4d6382bd0070bb5d9ef9
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-02-24 14:58:55 +0200

    load css file in js file in browser

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -20,22 +20,39 @@ export const fetchPlugin = (inputCode: string) => {
             contents: inputCode,
           };
         }
-        //check to see if we have already fetched this file and if it is in the cache
-        const cacheResult = await fileCache.getItem<esbuild.OnLoadResult>(
-          args.path
-        );
-        //if it is, return it immediately
-        if (cacheResult) {
-          return cacheResult;
-        }
+        // //check to see if we have already fetched this file and if it is in the cache
+        // const cacheResult = await fileCache.getItem<esbuild.OnLoadResult>(
+        //   args.path
+        // );
+        // //if it is, return it immediately
+        // if (cacheResult) {
+        //   return cacheResult;
+        // }
         const { data, request } = await axios.get(args.path);
-        //store response in cache
+        //loading CSS files
+        const fileType = args.path.match(/.css$/) ? "css" : "jsx";
+        //replace new line char,escape " and '
+        const escaped = data
+          .replace(/\n/g, "")
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+        //put css in js file
+        const contents =
+          fileType === "css"
+            ? `
+            const style = document.createElement('style');
+            style.innerText = '${escaped}';
+            document.head.appendChild(style);
+          `
+            : data;
+        //loaders for files
 
         const result: esbuild.OnLoadResult = {
           loader: "jsx",
-          contents: data,
+          contents: contents,
           resolveDir: new URL("./", request.responseURL).pathname,
         };
+        //store response in cache
 
         await fileCache.setItem(args.path, result);
         return result;

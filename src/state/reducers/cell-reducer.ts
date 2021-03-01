bb2ba5190111e6c8c8ee104b96365fcf9158ace9
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-03-01 13:21:55 +0200

    reducers whith immer and testing reducers in store

--- src/state/reducers/cell-reducer.ts
+++ src/state/reducers/cell-reducer.ts
@@ -2,6 +2,11 @@ import produce from "immer";
 import { ActionType } from "../action-types";
 import { Action } from "../actions";
 import { Cell } from "../cell";
+
+//create random id
+const randomId = () => {
+  return Math.random().toString(36).substr(2, 5);
+};
 //output of the reducer
 interface CellState {
   data: {
@@ -20,20 +25,53 @@ const initialState: CellState = {
 };
 
 //https://immerjs.github.io/immer/docs/update-patterns
+//returning state just for TS, otherwise TS will tink that state can be undefined immer is taking care to return what we want
 const reducer = produce((state: CellState = initialState, action: Action) => {
   switch (action.type) {
     case ActionType.DELETE_CELL:
       const idToDelete = action.payload;
       delete state.data[idToDelete];
       state.order = state.order.filter((x) => x !== idToDelete);
-      return;
+      return state;
     case ActionType.UPDATE_CELL:
       const { id, content } = action.payload;
       state.data[id].content = content;
-      return;
+      return state;
     case ActionType.INSERT_CELL_BEFORE:
+      //create new Cell
+      const cell: Cell = {
+        content: "",
+        type: action.payload.type,
+        id: randomId(),
+      };
+      //create new cell at state.data
+      state.data[cell.id] = cell;
+
+      const foundIndex = state.order.findIndex(
+        (id) => id === action.payload.id
+      );
+      console.log(foundIndex);
+      if (foundIndex < 0) {
+        state.order = state.order.concat(cell.id);
+      } else {
+        //insert in order arr
+        state.order.splice(foundIndex, 0, cell.id);
+      }
       return state;
     case ActionType.MOVE_CELL:
+      const { direction } = action.payload;
+      const indexToMove = state.order.findIndex(
+        (id) => id === action.payload.id
+      );
+      let newIndex = direction === "up" ? indexToMove - 1 : indexToMove + 1;
+      if (newIndex >= state.order.length) {
+        newIndex = state.order.length - 1;
+      } else if (newIndex < 0) {
+        newIndex = 0;
+      }
+      //swap places
+      state.order[indexToMove] = state.order[newIndex];
+      state.order[newIndex] = action.payload.id;
       return state;
     default:
       return state;
--- src/state/store.ts
+++ src/state/store.ts
@@ -1,5 +1,41 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import reducers from "./reducers";
+import { ActionType } from "./action-types";
 
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+//test reducers
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: { id: null, type: "code" },
+});
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: { id: null, type: "text" },
+});
+console.log(store.getState());
+const tempId = store.getState().cells.order[1];
+console.log(tempId);
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: { id: tempId, type: "code" },
+});
+console.log(store.getState());
+store.dispatch({
+  type: ActionType.DELETE_CELL,
+  payload: tempId,
+});
+console.log(store.getState());
+const tempId2 = store.getState().cells.order[1];
+
+store.dispatch({
+  type: ActionType.MOVE_CELL,
+  payload: { id: tempId2, direction: "up" },
+});
+console.log(store.getState());
+store.dispatch({
+  type: ActionType.UPDATE_CELL,
+  payload: { id: tempId2, content: "change" },
+});
+console.log(store.getState());

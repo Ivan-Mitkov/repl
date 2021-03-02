863f6d2b226b0e39f9015a6b42bedfaf2f32307c
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-03-02 12:14:07 +0200

    add cells

--- src/components/CellList.tsx
+++ src/components/CellList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useTypedSelector } from "../hooks/useTypedSelector";
 import CellListItem from "./CellListItem";
+import AddCell from "./AddCell";
 
 const CellList: React.FC = () => {
   //get all cells from state in an ordered list
@@ -9,10 +10,18 @@ const CellList: React.FC = () => {
   });
 
   const reneredCells = cells.map((cell) => (
-    <CellListItem cell={cell} key={cell.id}/>
+    <React.Fragment key={cell.id}>
+      <AddCell nextCellId={cell.id} />
+      <CellListItem cell={cell} />
+    </React.Fragment>
   ));
 
-  return <div>{reneredCells}</div>;
+  return (
+    <div>
+      {reneredCells}
+      <AddCell forceVisible={cells.length === 0} nextCellId={null} />
+    </div>
+  );
 };
 
 export default CellList;
